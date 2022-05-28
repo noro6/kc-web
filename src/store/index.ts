f69c8e849a40e9573754369ded22e279ec39fc09
noro6 <noro_next@outlook.jp>
2022-05-28 17:01:15 +0900

    バージョン更新

--- src/store/index.ts
+++ src/store/index.ts
@@ -24,7 +24,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    siteVersion: '2.7.5',
+    siteVersion: '2.7.6',
     items: [] as ItemMaster[],
     ships: [] as ShipMaster[],
     cells: [] as CellMaster[],
