04ac46b1939beefc9459b0e436450450573538a3
noro6 <noro_next@outlook.jp>
2022-08-31 23:30:07 +0900

    特効更新

--- src/classes/const.ts
+++ src/classes/const.ts
@@ -170,12 +170,13 @@ export default class Const {
     { text: 'A', subText: '2', items: [459, 242, 256, 257, 233, 277, 197, 198, 205, 206, 254, 255] },
     { text: 'A', subText: '3', items: [431, 432, 243, 188, 316, 248, 64, 305, 306, 422] },
     { text: 'A', subText: '4', items: [405, 433, 244, 424, 425] },
+    { text: 'B', subText: '1', items: [178, 354] },
     { text: 'B', subText: '2', items: [158, 159, 353] },
     { text: 'B', subText: '3', items: [249, 184] },
     { text: 'B', subText: '4', items: [189] },
     { text: 'C', subText: '1', items: [250, 251, 253] },
     { text: 'C', subText: '3', items: [473] },
-    { text: 'C', subText: '4', items: [252, 434, 435, 423] },
+    { text: 'C', subText: '4', items: [252, 434, 435, 423, 479] },
   ];
 
   public static readonly ITEM_API_TYPE = [
