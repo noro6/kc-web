e499876094f37513d2a37eb13b0c2f9fcebff6d2
noro6 <noro_next@outlook.jp>
2023-01-08 18:12:49 +0900

    差戻し

--- src/store/index.ts
+++ src/store/index.ts
@@ -25,7 +25,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    siteVersion: '2.33.10',
+    siteVersion: '2.33.9',
     items: [] as ItemMaster[],
     ships: [] as ShipMaster[],
     cells: [] as CellMaster[],
