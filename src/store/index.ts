dc638acd1349ca5b75c8e33747e22ff3608344eb
noro6 <noro_next@outlook.jp>
2023-05-07 12:02:29 +0900

    ライブラリ更新

--- src/store/index.ts
+++ src/store/index.ts
@@ -25,7 +25,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     /** サイトバージョン */
-    siteVersion: '2.37.3',
+    siteVersion: '2.37.4',
     /** 装備マスタデータ */
     items: [] as ItemMaster[],
     /** 艦船マスタデータ */
