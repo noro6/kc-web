4fd0b6b837fa0785a31b174eb589969820ef19d6
noro6 <noro_next@outlook.jp>
2023-06-14 22:30:48 +0900

    時雨改三対応

--- src/store/index.ts
+++ src/store/index.ts
@@ -25,7 +25,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     /** サイトバージョン */
-    siteVersion: '2.39.1',
+    siteVersion: '2.39.2',
     /** 装備マスタデータ */
     items: [] as ItemMaster[],
     /** 艦船マスタデータ */
