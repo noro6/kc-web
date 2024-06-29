459fc3b7513dbc2f143ab21b47e60fbab814cb90
noro6 <noro_next@outlook.jp>
2024-06-29 21:22:07 +0900

    初月改二装備制限修正

--- src/classes/const.ts
+++ src/classes/const.ts
@@ -518,9 +518,9 @@ export default class Const {
     {
       shipId: 724, index: [1, 2, 3, 4, 5], itemType: [0], itemIDs: [26, 62, 79, 80, 81, 207, 208, 237, 322, 323, 367, 368, 369, 490],
     },
-    // 初月改二 4スロットに 主砲、魚雷系 不可
+    // 初月改二 4スロットに 主砲、魚雷、大型電探系 不可
     {
-      shipId: 968, index: [4], itemType: [1, 5], itemIDs: [0],
+      shipId: 968, index: [4], itemType: [1, 5, 13], itemIDs: [0],
     },
   ];
 
