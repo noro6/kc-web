f78953ff1982cdb49730f92caafc9a167f2996eb
noro6 <noro_next@outlook.jp>
2024-09-20 00:56:22 +0900

    特効更新

--- src/store/index.ts
+++ src/store/index.ts
@@ -28,7 +28,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     /** サイトバージョン */
-    siteVersion: '2.48.21',
+    siteVersion: '2.48.22',
     /** 装備マスタデータ */
     items: [] as ItemMaster[],
     /** 艦船マスタデータ */
