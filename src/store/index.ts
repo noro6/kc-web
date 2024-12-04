83a60cb51abb14613d152121fd9fca71089932c3
noro6 <noro_next@outlook.jp>
2024-12-04 20:29:56 +0900

    照明弾アイコン差し替え

--- src/store/index.ts
+++ src/store/index.ts
@@ -28,7 +28,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     /** サイトバージョン */
-    siteVersion: '2.48.31',
+    siteVersion: '2.48.32',
     /** 装備マスタデータ */
     items: [] as ItemMaster[],
     /** 艦船マスタデータ */
